1d6d8c70510a338f81a06e6d053d732a7cef81f6
sylvia <sylvias821205@gmail.com>
2026-08-23 18:24:00 +0800

    Refactor: InMemoryCustomerRepository to Data

--- CustomerManager.Data/InMemoryCustomerRepository.cs
+++ CustomerManager.Data/InMemoryCustomerRepository.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CustomerManager.Core;
 
-namespace CustomerManager.Core
+namespace CustomerManager.Data
 {
 	public class InMemoryCustomerRepository : ICustomerRepository
 	{
